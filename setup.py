023430f6cc2db6e144df7613079ff1baf8717c4f
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-01-19 01:36:38 +0100

    add data files entry to setup.py to property include firefox extensions

--- setup.py
+++ setup.py
@@ -20,6 +20,11 @@ setup(
         'fusepy==3.0.*',
         'psutil==4.3.*',
     ],
+    data_files=[('extensions', [
+        'extensions/idcac_2.9.5.xpi',
+        'extensions/referer_header.xpi',
+        'extensions/uBlock0_1.17.7rc0.xpi',
+    ])],
     entry_points={
         'console_scripts': [
             'saas = saas.saas:main'
