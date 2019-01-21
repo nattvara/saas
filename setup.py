e0899e8a45878af1d7fa1cf6ed78907c5ee5722b
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2019-01-21 01:16:06 +0100

    add long description and github url

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 """Setup."""
 
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+readme = path.join(this_directory, 'README.md')
+with open(readme, encoding='utf-8') as file:
+    long_description = file.read()
 
 setup(
     name='saas',
     version='1.0',
     description='Screenshot as a service',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ludwig Kristoffersson',
     author_email='ludwig@kristoffersson.org',
+    url='https://github.com/nattvara/saas',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
